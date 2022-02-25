025d74d0918c514611f9f7a397b0ab8da144d7c6
albert <albertkingdom@gmail.com>
2022-02-25 15:50:01 +0800

    use connection pool

--- db.js
+++ db.js
@@ -3,18 +3,28 @@ var mysql = require("mysql2");
 require("dotenv").config();
 
 // mysql2 setting
-var db = mysql.createConnection({
+// var db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+// });
+
+// db.connect((err) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log("db connected!");
+// });
+
+// connection pool
+var db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-});
-
-db.connect((err) => {
-  if (err) {
-    throw err;
-  }
-  console.log("db connected!");
+  waitForConnections: true,
+  queueLimit: 0,
 });
 
 module.exports = db;
